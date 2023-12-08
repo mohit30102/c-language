10f7b7dd011c36db8db7a4158eb30907942d8c41
mohit30102 <152309964+mohit30102@users.noreply.github.com>
2023-12-08 14:19:23 +0530

    Add files via upload

---
+++ c ++/Greenhorns/swap.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+
+int main(){
+  int a,b;
+	printf("enter value a & b: ");
+	scanf(" %d %d" ,&a ,&b);
+  //a=120
+  //b=100
+  a=a+b;//220
+  b=a-b;//220-100=120
+  a=a-b;//120-220=100
+	printf("\n result x  %d",a);
+	printf("\n resulty %d",b);
+  return 0;
+}
